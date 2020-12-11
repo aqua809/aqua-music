cb9f909f48ed63420f08a3b7fe35cc6d285d385e
aqua809 <72797332+aqua809@users.noreply.github.com>
2020-12-11 12:50:17 +0530

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -15,8 +15,8 @@ module.exports = {
     //react with approve emoji
     message.react("769665713124016128");
     //send the invite embed
-    message.reply(new MessageEmbed().setColor("#c219d8").setTitle(":heart: Please Invite me: ").setDescription("https://discord.com/oauth2/authorize?client_id=769642999227351070&permissions=0&scope=bot")
-    .setFooter("And enjoy listening to music!", "https://cdn.discordapp.com/avatars/769642999227351070/f1b78891507308fb76c0a66b56f4bcd6.webp?size=512"));
+    message.reply(new MessageEmbed().setColor("#c219d8").setTitle(":heart: Please Invite me: ").setDescription("https://discord.com/oauth2/authorize?client_id=771222165558263829&scope=bot&permissions=2147483647")
+    .setFooter("And enjoy listening to music!", "https://cdn.discordapp.com/avatars/771222165558263829/82293edd5dcc0375d34514a1ebae3c99.png?size=1024"));
 
   }
 }
